f22d3412bb31c416a26ccef0c07add378b68d319
Deeksha kashyap <deekshakashyap01@gmail.com>
2019-06-27 22:21:15 -0700

    take a input

--- src/main/Sample.java
+++ src/main/Sample.java
@@ -6,5 +6,7 @@ public class Sample {
     public static void main(String[] args) {
         int i,j,b=1,a,n;
         Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the number");
+        n=scan.nextInt();
     }
 }

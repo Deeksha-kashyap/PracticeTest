423108d211559af5d9d7b8974e004fa3a1d62bf0
Deeksha kashyap <deekshakashyap01@gmail.com>
2019-06-27 22:19:15 -0700

    use a Scanner to input the value

---
+++ src/main/Sample.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.util.Scanner;
+
+public class Sample {
+    public static void main(String[] args) {
+        int i,j,b=1,a,n;
+        Scanner scan=new Scanner(System.in);
+    }
+}

cf3e3c9f07ba938cc357f12828cc17230ae27821
Deeksha kashyap <deekshakashyap01@gmail.com>
2019-06-27 22:23:51 -0700

    make a loop for print the value

--- src/main/Sample.java
+++ src/main/Sample.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 
 public class Sample {
     public static void main(String[] args) {
-        int i,j,b=1,a,n;
+        int b=1,a,n;
         Scanner scan=new Scanner(System.in);
         System.out.println("Enter the number");
         n=scan.nextInt();
+        for (int i = 1; i <=n ; i++) {
+            a=b;
+            for (int j = 1; j <=i ; j++) {
+                System.out.println(a);
+                a--;
+            }
+        }
     }
 }

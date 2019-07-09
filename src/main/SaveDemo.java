07e2e91126c1c50e4283f04a751c431687bbd991
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-09 21:35:17 +1300

    Program of serializable with student class

--- src/main/SaveDemo.java
+++ src/main/SaveDemo.java
@@ -1,4 +1,21 @@
 package main;
 
+import java.io.*;
 public class SaveDemo {
+    public static void main(String[] args) throws IOException {
+        Student s1=new Student(100,"Rajat");
+        FileOutputStream fout=new FileOutputStream("f.txt");
+        ObjectOutputStream out=new ObjectOutputStream(fout);
+        try {
+            out.writeObject(s1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Serializable is done successfully");
+    }
 }

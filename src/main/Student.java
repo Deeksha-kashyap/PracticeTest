767780e6d0aa8aeec7748f61da53722f36944789
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-09 21:25:16 +1300

    make a method Student with two parameter

--- src/main/Student.java
+++ src/main/Student.java
@@ -1,7 +1,12 @@
 package main;
 
 import java.io.Serializable;
-public class Student {
+public class Student implements Serializable {
 int id;
 String name;
+
+public Student(int id,String name)
+{
+
+}
 }

ddba81f3a6c4e13d14bb937ca9606ab68c6530b0
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-09 21:39:07 +1300

    create the objectinputStream

--- src/main/ReadDemo.java
+++ src/main/ReadDemo.java
@@ -1,4 +1,8 @@
 package main;
 
+import java.io.*;
 public class ReadDemo {
+    public static void main(String[] args) {
+        ObjectOutputStream in=new ObjectOutputStream(new FileInputStream("MyFile.txt"));
+    }
 }

af4c41da659074d290e1d790ada2cf55969af9dd
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 19:00:49 +1300

    using the Socket object with server with accept method.

--- src/main/SimpleServer.java
+++ src/main/SimpleServer.java
@@ -1,9 +1,12 @@
 package main;
 
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class SimpleServer {
     public static void main(String[] args) {
         ServerSocket server=new ServerSocket(5432);
+        System.out.println("Server is Ready wait for client");
+        Socket clientSocket=server.accept();
     }
 }

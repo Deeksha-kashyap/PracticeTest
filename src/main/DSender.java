bbf232fea6e4c86f8bdea8b5df7354f0904c9e66
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 20:09:21 +1300

    use the DatagramSocket as a object and also add the string.

---
+++ src/main/DSender.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.net.DatagramSocket;
+
+public class DSender {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket ds=new DatagramSocket();
+        String str="India is a good Country";
+    }
+}

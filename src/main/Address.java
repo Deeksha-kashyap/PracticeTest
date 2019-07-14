915b35c43ea0dd7aff3669543df16fc10cb50f98
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 18:28:03 +1300

    knowing the ip address of a website and also it needs internet connection

--- src/main/Address.java
+++ src/main/Address.java
@@ -6,5 +6,6 @@ import java.io.InputStreamReader;
 public class Address {
     public static void main(String[] args) {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a website name/computer name ");
     }
 }

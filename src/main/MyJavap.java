2615a7aa48840d848a4391cf477b6509868182d0
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 20:34:14 +1300

    make the Class type object with forName

---
+++ src/main/MyJavap.java
@@ -0,0 +1,8 @@
+package main;
+
+public class MyJavap {
+    public static void main(String[] args) throws Exception {
+        Class c=Class.forName(args[0]);
+        System.out.println("Fields........");
+    }
+}

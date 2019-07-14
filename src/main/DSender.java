cadb36c52a5a959d740b743b8aaed72880ce021d
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 20:14:46 +1300

    add the DatagramPacket as a object to call the length of string and InetAddress.

--- src/main/DSender.java
+++ src/main/DSender.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
@@ -9,5 +10,6 @@ public class DSender {
         String str="India is a good Country";
         InetAddress ia=InetAddress.getByName("localhost");
         byte[] barr=str.getBytes();
+        DatagramPacket dp=new DatagramPacket(barr,str.length(),ia,3000);
     }
 }

5da713cfffa5e513231e457e233c34c5aabe8d52
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 19:03:28 +1300

    get output stream associated with the socket.

--- src/main/SimpleServer.java
+++ src/main/SimpleServer.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,5 +9,7 @@ public class SimpleServer {
         ServerSocket server=new ServerSocket(5432);
         System.out.println("Server is Ready wait for client");
         Socket clientSocket=server.accept();
+        System.out.println("Connection is established with the client");
+        OutputStream serverOut=clientSocket.getOutputStream();
     }
 }

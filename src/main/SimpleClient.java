050f069e30efb33d49e945ea2b9a32fdb67a07e7
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 19:15:52 +1300

    using  InputStream and BufferedReader with InputStreamReader

--- src/main/SimpleClient.java
+++ src/main/SimpleClient.java
@@ -1,10 +1,16 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class SimpleClient {
     public static void main(String[] args)throws Exception
     {
        Socket client=new Socket("localhost",5432);
+       //Socket client=new Socket(InetAddress.getLocalHost(1,5432));
+        InputStream is=client.getInputStream();
+        BufferedReader br=new BufferedReader(new InputStreamReader(is));
     }
 }

469baf734704d64268fa16a1c5f4220638c75b51
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 18:25:58 +1300

    call the buffered reader object and input stream reader in system

---
+++ src/main/Address.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Address {
+    public static void main(String[] args) {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    }
+}

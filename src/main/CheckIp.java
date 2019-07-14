d9fdd8b79ff46112328dc63cf34f5c2372ac1ce7
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 18:21:24 +1300

    use the try catch exception for to print name and ip

--- src/main/CheckIp.java
+++ src/main/CheckIp.java
@@ -1,12 +1,17 @@
 package main;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class CheckIp {
     public static void main(String[] args) {
         try{
             InetAddress address=InetAddress.getLocalHost();
-            System.out.println(address);
+            System.out.println(address); //to print name and ip
+        }
+        catch(UnknownHostException e)
+        {
+            System.out.println(e);
         }
     }
 }

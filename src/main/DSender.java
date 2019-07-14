445d5d9b9f088c020bc340611a8d99065116fcc4
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 20:11:09 +1300

    use InetAddress as a object by getByName method

--- src/main/DSender.java
+++ src/main/DSender.java
@@ -1,10 +1,12 @@
 package main;
 
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class DSender {
     public static void main(String[] args) throws Exception {
         DatagramSocket ds=new DatagramSocket();
         String str="India is a good Country";
+        InetAddress ia=InetAddress.getByName("localhost");
     }
 }

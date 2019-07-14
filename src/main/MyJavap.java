0577412cc47d8437c74fb81609565a7ac28ceef9
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 20:35:56 +1300

    make the Field type object and Declared the Fields by c

--- src/main/MyJavap.java
+++ src/main/MyJavap.java
@@ -1,8 +1,12 @@
 package main;
 
+
+import java.lang.reflect.*;
+
 public class MyJavap {
     public static void main(String[] args) throws Exception {
         Class c=Class.forName(args[0]);
         System.out.println("Fields........");
+        Field f[]=c.getDeclaredFields();
     }
 }

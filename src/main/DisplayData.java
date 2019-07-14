a05be3081e9dfb1e944986ee759b8efa39104a4b
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 19:53:01 +1300

    connect the URL by URLConnection and call url by openConnection and Input the Stream and call urlcon by getInputStream

--- src/main/DisplayData.java
+++ src/main/DisplayData.java
@@ -1,12 +1,16 @@
 package main;
 
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class DisplayData {
     public static void main(String[] args) {
         try
         {
             URL url=new URL("https://github.com/Deeksha-kashyap/C-sharp-Programs");
+            URLConnection urlcon=url.openConnection();
+            InputStream stream=urlcon.getInputStream();
         }
         catch (Exception e)
         {

ef435cf9ea406149b4a359c6a89ae0c83e1acd2e
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 20:20:18 +1300

    create the byte type array and make a DatagramSocket as a object.

---
+++ src/main/DReceiver.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.net.DatagramSocket;
+
+public class DReceiver {
+    public static void main(String[] args)throws Exception {
+        DatagramSocket ds=new DatagramSocket(3000);
+        byte[] buf=new byte[1024];
+    }
+}

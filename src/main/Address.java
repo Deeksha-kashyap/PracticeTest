755fde8fff0721a9ae707fc60a7b7c42ec699613
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 18:29:41 +1300

    getByName accept the site name and return the ip address

--- src/main/Address.java
+++ src/main/Address.java
@@ -7,5 +7,9 @@ public class Address {
     public static void main(String[] args) {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter a website name/computer name ");
+        try
+        {
+            String name=br.readLine();
+        }
     }
 }

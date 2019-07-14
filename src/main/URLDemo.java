8527c3165c10dad50a50d99f2750cf74cf5496c5
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 19:28:47 +1300

    print the port number by getPort method

--- src/main/URLDemo.java
+++ src/main/URLDemo.java
@@ -9,6 +9,7 @@ public class URLDemo {
             URL url=new URL("https://github.com");
             System.out.println("Protocol:"+url.getProtocol());
             System.out.println("Host Name:"+url.getHost());
+            System.out.println("Port Number:"+url.getPort());
         }
         catch(Exception e)
         {

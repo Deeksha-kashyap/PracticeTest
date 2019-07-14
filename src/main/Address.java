f9eaee562740436680c00eae3fa37d7aa148381a
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 18:33:37 +1300

    print the ip address

--- src/main/Address.java
+++ src/main/Address.java
@@ -3,6 +3,7 @@ package main;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.rmi.UnknownHostException;
 
 public class Address {
     public static void main(String[] args) {
@@ -12,6 +13,11 @@ public class Address {
         {
             String name=br.readLine();
             InetAddress ip=InetAddress.getByName(name);
+            System.out.println("The ip Address is"+ip);
+        }
+        catch(UnknownHostException e)
+        {
+
         }
     }
 }

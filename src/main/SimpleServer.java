200535b20bad947b7f036989c07641ba6d1ff866
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-07-14 18:57:12 +1300

    make ServerSocket object and give the port number

---
+++ src/main/SimpleServer.java
@@ -0,0 +1,9 @@
+package main;
+
+import java.net.ServerSocket;
+
+public class SimpleServer {
+    public static void main(String[] args) {
+        ServerSocket server=new ServerSocket(5432);
+    }
+}
